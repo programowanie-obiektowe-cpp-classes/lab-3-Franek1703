71bc5641be71af4ecaf48306eb8f5edc276f60dd
FRANCISZEK ZAREBSKI <fran.zarebski@gmail.com>
2024-11-04 17:20:01 +0100

    Add transportujFlota function

--- transportujFlota.hpp
+++ transportujFlota.hpp
@@ -4,6 +4,24 @@
 
 unsigned int transportujFlota(unsigned int towar)
 {
-    // Twoja implementacja tutaj
-    return 0;
+    unsigned int przetransportowanyTowar = 0;
+    unsigned int liczbaZaglowcow = 0;
+    Stocznia stocznia;
+    Stocznia::resetTotalCap();
+
+    while (przetransportowanyTowar < towar) {
+        Statek* statek = stocznia();
+        unsigned int ladunek = statek->transportuj();
+        przetransportowanyTowar += ladunek;
+
+        // Sprawdzamy, czy statek jest typu Zaglowiec
+        if (dynamic_cast<Zaglowiec*>(statek)) {
+            ++liczbaZaglowcow;
+        }
+
+        delete statek;
+
+    }
+    std::cout << "Całkowita pojemność przetransportowana przez flotę: " << Stocznia::getTotalCap() << std::endl;
+    return liczbaZaglowcow;
 }
